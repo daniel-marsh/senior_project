ab2b42a6013119a4266864124ce1e5addc1b0312
Daniel Marsh <danielmarsh@Daniels-MacBook-Pro.local>
2021-03-06 14:13:31 -0500

    Created third simple agent

--- cant_stop.cpp
+++ cant_stop.cpp
@@ -4,6 +4,7 @@
 #include "board.h"
 #include "random.h"
 #include "simple.h"
+#include "flat_mcts.h"
 using namespace std; 
 
 int main(int argc, char** argv) {
@@ -23,6 +24,9 @@ int main(int argc, char** argv) {
     else if ((strcmp(argv[2], "simple") == 0) or (strcmp(argv[2], "-s") == 0)) {
         make_player0_move = make_smart_move;
     }
+    else if ((strcmp(argv[2], "mcts") == 0) or (strcmp(argv[2], "-m") == 0)) {
+        make_player0_move = make_mcts_move;
+    }
 
     if ((strcmp(argv[3], "random") == 0) or (strcmp(argv[3], "-r") == 0)) {
         make_player1_move = make_random_move;
@@ -30,17 +34,25 @@ int main(int argc, char** argv) {
     else if ((strcmp(argv[3], "simple") == 0) or (strcmp(argv[3], "-s") == 0)) {
         make_player1_move = make_smart_move;
     }
+    else if ((strcmp(argv[3], "mcts") == 0) or (strcmp(argv[3], "-m") == 0)) {
+        make_player1_move = make_mcts_move;
+    }
 
     srand(time(0));
     Board game_board;
     game_board.init(dice_size);
+    int count = 1;
     while (game_board.game_over() < 0) {
+        if (count % 10 == 0) {
+            std::cout << "Move Number " << count << "\n";
+        }
         if (game_board.turn == 0) {
             game_board = make_player0_move(game_board);
         }
         else {
             game_board = make_player1_move(game_board);
         }
+        count++;
     }
     int winner = game_board.game_over();
     // game_board = make_random_move(game_board);
